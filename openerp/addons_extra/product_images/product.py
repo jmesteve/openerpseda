a92f83eb946c3ca04b9543da34ff8eebb0796640
Jose miguel Esteve esteve <jmesteveme.com>
2014-01-03 03:01:47 +0100

    delete addons connector

--- openerp/addons_extra/product_images/product.py
+++ openerp/addons_extra/product_images/product.py
@@ -78,7 +78,10 @@ class product_product(orm.Model):
         if image.link:
             if image.url:
                 config = openerp.tools.config
-                host = 'http://'+ config['netrpc_interface']+':'+ str(config['xmlrpc_port'])
+                if config['netrpc_interface'] and config['xmlrpc_port']:
+                    host = 'http://'+ config['netrpc_interface']+':'+ str(config['xmlrpc_port'])
+                else:
+                    return False
                 url = host + image.url
                 (filename, header) = urllib.urlretrieve(url)
                 with open(filename, 'rb') as f:
