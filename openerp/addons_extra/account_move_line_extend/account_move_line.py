da4009224b3a5b3568e09d085cd186d12289ef07
Jose miguel Esteve esteve <jmesteveme.com>
2014-01-27 00:12:16 +0100

    balance

--- openerp/addons_extra/account_move_line_extend/account_move_line.py
+++ openerp/addons_extra/account_move_line_extend/account_move_line.py
@@ -1,5 +1,5 @@
 from openerp.osv import fields, osv, orm
-
+from openerp import pooler
 
 
 
@@ -15,25 +15,37 @@ class account_move_line(osv.osv):
             accumulated += debit - credit
             res[line.id] = accumulated
         return res
+    def _get_balance(self, cr, uid, ids,name, unknow_none, context=None):
+        res = dict.fromkeys(ids, False)
+        for line in self.browse(cr, uid, ids, context=context):
+            debit = line.debit
+            credit = line.credit
+            res[line.id] = credit - debit
+        return res
+    
     _columns = {
-                'accumulated': fields.function(_get_accumulated, type='float', string='accumulated'),
-               
+                'balance': fields.function(_get_balance, type='float',digits=(16,2), string='Balance'),
+                'accumulated': fields.function(_get_accumulated, type='float', digits=(16,2), string='accumulated'),
                 }
     _order = "date, id"
     
-    #def search(self, cr, uid, args, offset=0, limit=None, order=None, context=None, count=False):
-    #    res = super(account_move_line, self).search(cr, uid, args, offset, limit, order, context, count)
-    #    
-    #    lines ={}
-    #    accumulated = 0
-    #    for line in self.browse(cr, uid, res, context=context):
-    #        debit = line.debit
-    #        credit = line.credit
-    #        accumulated += debit - credit
-    #        lines[line.id] = accumulated
-    #    self.write(cr, uid,res, {
-    #                            'accumulated':lines,
-    #                        })
-    #    return res
+    def list_accounts(self, cr, uid, context=None):
+        ids = self.pool.get('account.account').search(cr,uid,[],0, None,('code'))
+        lines = self.pool.get('account.account').browse(cr, uid, ids, context)
+        res = []
+        for line in lines:
+            length  = 9 - len(line.code.strip())
+            spaces = '&nbsp;' * length *2 + '&nbsp;'
+            description = line.code + spaces + line.name
+            res.append((line.id,description,line.code))
+        #names = self.pool.get('account.account').name_get(cr, uid, ids, context)
+        return res
+    
+    def print_extract(self, cr, uid, context=None):
+        win_obj = self.pool.get('ir.actions.act_window')
+        res = win_obj.for_xml_id(cr, uid, 'account', 'account_report_general_ledger_view', context)
+        return res
+        
+
     
 account_move_line()
--- openerp/addons_extra/account_move_line_extend/static/src/js/account_move_line_extend.js
+++ openerp/addons_extra/account_move_line_extend/static/src/js/account_move_line_extend.js
@@ -11,14 +11,17 @@ openerp.account.quickadd = function (instance) {
             this._super.apply(this, arguments);
             this.journals = [];
             this.periods = [];
+            this.accounts =[];
+            this.current_account = null;
             this.current_journal = null;
             this.current_period = null;
+            this.default_account = null;
             this.default_period = null;
             this.default_journal = null;
             this.current_journal_type = null;
             this.current_journal_currency = null;
             this.current_journal_analytic = null;
-            this.current_account = null;
+            
         },
         start:function(){
             var tmp = this._super.apply(this, arguments);
@@ -36,21 +39,31 @@ openerp.account.quickadd = function (instance) {
             
             this.$el.parent().find('.oe_account_select_account').change(function() {
             	self.current_account = this.value === '' ? null : parseInt(this.value);
-                //self.do_search(self.last_domain, self.last_context, self.last_group_by);
+                self.do_search(self.last_domain, self.last_context, self.last_group_by);
+            });
+            
+            this.$el.parent().find('.print_extract_button').click(function() {
+            	mod.call("print_extract", []).then(function(result) {
+                    console.log( result) ;
+                });
             });
             
             this.on('edit:after', this, function () {
                 self.$el.parent().find('.oe_account_select_journal').attr('disabled', 'disabled');
                 self.$el.parent().find('.oe_account_select_period').attr('disabled', 'disabled');
+                self.$el.parent().find('.oe_account_select_account').attr('disabled', 'disabled');
             });
             this.on('save:after cancel:after', this, function () {
                 self.$el.parent().find('.oe_account_select_journal').removeAttr('disabled');
                 self.$el.parent().find('.oe_account_select_period').removeAttr('disabled');
+                self.$el.parent().find('.oe_account_select_account').removeAttr('disabled');
             });
             var mod = new instance.web.Model("account.move.line", self.dataset.context, self.dataset.domain);
-            mod.call("default_get", [['journal_id','period_id'],self.dataset.context]).then(function(result) {
+            
+            mod.call("default_get", [['journal_id','period_id','account_id'],self.dataset.context]).then(function(result) {
                 self.current_period = result['period_id'];
-                self.current_journal = result['journal_id'];
+                //self.current_journal = result['journal_id'];
+                //self.current_account = result['account_id'];
             });
             return tmp;
         },
@@ -65,18 +78,23 @@ openerp.account.quickadd = function (instance) {
                 self.journals = result;
             }),mod.call("list_periods", []).then(function(result) {
                 self.periods = result;
+            }),mod.call("list_accounts", []).then(function(result) {
+                self.accounts = result;
             })).then(function () {
                 var o;
                 
                 self.$el.parent().find('.oe_account_select_account').children().remove().end();
                 self.$el.parent().find('.oe_account_select_account').append(new Option('', ''));
-                o = new Option("570000001", "Caja");
-                self.$el.parent().find('.oe_account_select_account').append(o);
-                
-                
+                for (var i = 0;i < self.accounts.length;i++){
+                    //o = new Option(self.accounts[i][1], self.accounts[i][0]);
+                    o = "<option value="+self.accounts[i][0]+">"+self.accounts[i][1]+"</option>"
+                    self.$el.parent().find('.oe_account_select_account').append(o);
+                } 
+                self.$el.parent().find('.oe_account_select_account').val(self.current_account).attr('selected',true);
                 
                 self.$el.parent().find('.oe_account_select_journal').children().remove().end();
                 self.$el.parent().find('.oe_account_select_journal').append(new Option('', ''));
+                console.log(self.current_journal);
                 for (var i = 0;i < self.journals.length;i++){
                     o = new Option(self.journals[i][1], self.journals[i][0]);
                     if (self.journals[i][0] === self.current_journal){
@@ -87,6 +105,7 @@ openerp.account.quickadd = function (instance) {
                     }
                     self.$el.parent().find('.oe_account_select_journal').append(o);
                 }
+                
                 self.$el.parent().find('.oe_account_select_period').children().remove().end();
                 self.$el.parent().find('.oe_account_select_period').append(new Option('', ''));
                 for (var i = 0;i < self.periods.length;i++){
@@ -94,20 +113,58 @@ openerp.account.quickadd = function (instance) {
                     self.$el.parent().find('.oe_account_select_period').append(o);
                 }    
                 self.$el.parent().find('.oe_account_select_period').val(self.current_period).attr('selected',true);
+                
                 return self.search_by_journal_period();
             });
+        },
+        search_accounts: function(id){
+        	var self = this;
+        	console.log(self.accounts);
+        	var size = self.accounts.length;
+        	var account = -1;
+        	for(var i=0; i<size; i++){
+        		var line = self.accounts[i]; 
+        		if(id == line[0]){
+        			account = line[2];
+        		}
+        	}
+        	var account_size = account.length;
+        	var res = [];
+        	for(var i=0; i<size; i++){
+        		var line = self.accounts[i]; 
+        		if(account == line[2].substr(0,account_size) ){
+        			res.push(line[2]);
+        		}
+        	}
+        	//console.log(res);	
+        	return res;
         },
         search_by_journal_period: function() {
             var self = this;
             var domain = [];
             if (self.current_journal !== null) domain.push(["journal_id", "=", self.current_journal]);
             if (self.current_period !== null) domain.push(["period_id", "=", self.current_period]);
+            if (self.current_account !== null) {
+            	var accounts_list = self.search_accounts(self.current_account);
+            	domain.push(["account_id", "child_of", accounts_list]);
+            }
+          
+            
             self.last_context["journal_id"] = self.current_journal === null ? false : self.current_journal;
             if (self.current_period === null) delete self.last_context["period_id"];
             else self.last_context["period_id"] =  self.current_period;
+            
+            if(self.current_account === null){
+            	delete self.last_context["account_id"];
+            }
+            else{
+            	self.last_context["account_id"] = self.current_account;
+            }
+            
             self.last_context["journal_type"] = self.current_journal_type;
             self.last_context["currency"] = self.current_journal_currency;
             self.last_context["analytic_journal_id"] = self.current_journal_analytic;
+            
             return self.old_search(new instance.web.CompoundDomain(self.last_domain, domain), self.last_context, self.last_group_by);
         },
     });
